753dc258a511f62625e22dbf0f3b830bd93cb526
emcaruso <emcaruso@unibz.it>
2025-06-05 15:56:01 +0200

    chk

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,9 +2,5 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 cd "$SCRIPT_PATH"
 
 cp ./.zshrc ~/.zshrc
-# exec zsh in .bashrc, if not present
-if ! grep -q "exec zsh" ~/.bashrc; then
-	echo "exec zsh" >>~/.bashrc
-fi
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
