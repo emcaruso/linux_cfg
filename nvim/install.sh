32b7f2685c96fa06534d916880cdd6f325c10bb3
emcaruso <emcaruso@unibz.it>
2025-06-10 10:51:08 +0200

    added lazygit installation

--- nvim/install.sh
+++ nvim/install.sh
@@ -11,3 +11,8 @@ cp -r ./config/ ~/.config/nvim
 # install neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# install lazygit
+curl -LO https://github.com/jesseduffield/lazygit/releases/download/v0.52.0/lazygit_0.52.0_Linux_x86_64.tar.gz
+tar -xzf lazygit_0.52.0_Linux_x86_64.tar.gz
+mv lazygit ~/.local/bin
